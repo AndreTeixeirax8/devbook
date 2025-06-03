e114c87408ee2fbe28bb542b03dc9693132f4229
André Teixeira <andre-x8@hotmail.com>
2025-06-02 21:13:20 -0300

    adiciona rota na controller

--- src/controllers/usuarios.go
+++ src/controllers/usuarios.go
@@ -1,9 +1,39 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/banco"
+	"api/src/modelos"
+	"api/src/repositorios"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+//REVISAR TODO O FLUXO PARA CRIAR USUARIO APENAS REVISAR PARA ENTENDER
 
 func CriarUsuario(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("criando um usuário"))
+	fmt.Println("--- Inicia função de criar usuario na controller")
+	corpoRequest, erro := ioutil.ReadAll(r.Body)
+
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	var usuario modelos.Usuario
+	if erro = json.Unmarshal(corpoRequest, &usuario); erro != nil {
+		log.Fatal(erro)
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	repositorio := repositorios.NovoRepositorioDeUsuarios(db)
+	repositorio.Criar((usuario))
+
 }
 
 func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
